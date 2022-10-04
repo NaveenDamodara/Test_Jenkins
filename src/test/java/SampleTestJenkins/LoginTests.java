dfb207ef64a8ba50fce1c47fac0338f61924e8f1
Naveen <damodaranaveen98@gmail.com>
2022-10-04 13:03:35 +0530

    intial

---
+++ src/test/java/SampleTestJenkins/LoginTests.java
@@ -0,0 +1,40 @@
+package SampleTestJenkins;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests{
+	
+	@Test
+	public void logindetailswithvaliedcredintials() {
+		
+		System.out.println("I am in jenkins project");
+		Assert.assertTrue(true);
+		System.out.println("I am in login test class");
+			
+	}
+	
+@Test	
+public void logindetailswithinvaliedcredintials() {
+		
+		System.out.println("username");
+		Assert.assertTrue(true);
+		System.out.println("password");
+			
+	}
+@Test	
+public void logindetailswithnovaliedcredintials() {
+	
+	System.out.println("worng username");
+	Assert.assertTrue(true);
+	System.out.println("worg password");
+		
+}	
+	
+	
+	
+	
+	
+	
+	
+}
